cc41920e3fcd44447a029ff33390c09c77b6d9b7
Christopher Yhap <christopheryhap@ChristophersAir.lan>
2024-12-09 17:01:40 -0500

    Solo.Project Day1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const app = express(); //get server
+const Entry = require('./models/entry.model.js'); //importing Entry schema
 
+app.use(express.json); //to use middleware
+
+//INITIALIZATION------------
 app.get('/', (req, res) => {
   res.send('Hello Christopher Yhap');
 });
-mongoose
+
+//GETTING ALL Entries ------------
+
+app.get('/api/entries', async (req, res) => {
+  try {
+    const entries = await Entry.find(req.body);
+    res.status(200).json(entries);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//POSTING----------
+app.post('/api/entries', async (req, res) => {
+  try {
+    const entry = await Entry.create(req.body);
+    res.status(200).json(entry);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose //connecting to DB
   .connect(
     'mongodb+srv://admin:0CgyY4VUOiGbyGrV@journals.xnrov.mongodb.net/?retryWrites=true&w=majority&appName=Journals'
   )
---
+++ models/entry.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const EntrySchema = mongoose.Schema(
+  {
+    date: {
+      type: Number,
+      required: [true, 'Please enter Date as 00/00/0000'],
+    },
+    feelings: {
+      type: String,
+      required: [false, 'What emotions are you feeling currently'],
+    },
+
+    entry: {
+      type: String,
+      required: [true, 'Tell Me Everything'],
+    },
+  },
+  {
+    timestamps: true, //shows the created at and updated at fields
+  }
+);
+
+const Entry = mongoose.model('Entries', EntrySchema);
+
+module.exports = Entry;
